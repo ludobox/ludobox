28de4525afb0b9e925ca15d66aee5f2018ea0f4a
Clément Renaud <clement.renaud@gmail.com>
2015-10-10 16:36:33 +0200

    init templating

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# ludobox-ui
+# Ludobox UI
+
 User Interface for Ludobox
+
+## How it works
+
+A python script will generate the following files : a list of all games + a folder containing info of each game.
+
+    python generate.py # generate folders tree
+
+Each game is structured as follow
+
+    /games/game/game-info.json
+    /games/game/game-files.zip # any files attached to the game
+    /games/game/index.html # generated html file
+
+## App structure
+
+* ```games``` : list all games
+* ```games/add``` : add new games
+* ```games/jeu1``` : display game named "jeu1"
+
+
+## Install dependencies
+
+    pip install -r requirements.txt
---
+++ generate.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from jinja2 import Environment, FileSystemLoader
+import os
+import json
+from slugify import slugify
+
+accepted_types=["jpg","png","gif", "stl", "pdf"]
+
+data_dir = os.path.join(os.getcwd(), "data")
+games_dir = os.path.join(os.getcwd(), "games")
+
+
+# Read templates from files
+env = Environment(loader=FileSystemLoader('templates'))
+main_template = env.get_template('main.html')
+single_template = env.get_template('single.html')
+index_template = env.get_template('index.html')
+
+# create games dir 
+if not os.path.exists(games_dir):
+    os.makedirs(games_dir)
+
+games = []
+
+# list all folders in "games"
+for path in os.listdir(data_dir): 
+    if os.path.isdir(os.path.join(data_dir, path)): # check all dir
+
+        # parse game dir 
+        data_path = os.path.join(data_dir, path)
+        print data_path
+
+        # load json
+        json_path = os.path.join(data_path, "info.json")
+        with open(json_path, "r") as json_file : 
+            game_data = json.load( json_file )
+        games.append(game_data)
+
+        # add permalink
+        game_data["slug"] = slugify(game_data["name"])
+
+        # create game dir 
+        game_path =  os.path.join(games_dir, game_data["slug"])
+        if not os.path.exists(game_path):
+            os.makedirs(game_path)
+
+        # render template
+        single = single_template.render(game_data)
+        single_index_path = os.path.join(game_path, "index.html")
+
+        # write game index.html
+        with open(single_index_path , "wb") as single_index :
+            single_index.write(single.encode('utf-8'))
+
+# write root index.html
+index = index_template.render({"games" : games}) # pass games as a dict to jinja2
+main_index_path = os.path.join(games_dir, "index.html")
+
+with open(main_index_path , "wb") as main_index :
+    main_index.write(index.encode('utf-8'))
