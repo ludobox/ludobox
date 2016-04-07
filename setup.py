9b849ee7940093fcd833998c14bce50369ffcdd6
Pierre-Yves Martin <pym.aldebaran@gmail.com>
2016-04-07 17:07:04 +0200

    Whitespace correction in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ except ImportError:
     from distutils.core import setup, find_packages
 
 # Leave the following line to match the regexp [0-9]*\.[0-9]*\.[0-9]*
-version = "0.0.1" # [major].[minor].[release]
+version = "0.0.1"  # [major].[minor].[release]
 
 # parse README
 with open('README.md') as readme_file:
@@ -34,33 +34,33 @@ with open('README.md') as readme_file:
 
 # parse requirements
 with open('requirements.txt') as f:
-    required = [x for x in f.read().splitlines()] 
+    required = [x for x in f.read().splitlines()]
 
 setup(
-      name = "ludobox",
-      packages = find_packages(exclude=['tests']) ,
-      version = version,
-      description = "Ludobox",
-      long_description = long_description,
-      author = "DCALK",
-      author_email = "info@ludobox.net",
-      url = "http://ludobox.net",
-      download_url = "http://github.com/ludobox/ludobox-ui",
+      name="ludobox",
+      packages=find_packages(exclude=['tests']),
+      version=version,
+      description="Ludobox",
+      long_description=long_description,
+      author="DCALK",
+      author_email="info@ludobox.net",
+      url="http://ludobox.net",
+      download_url="http://github.com/ludobox/ludobox-ui",
       include_package_data=True,
-      keywords = ["game", "public domain"],
+      keywords=["game", "public domain"],
       entry_points={
         'console_scripts': [
             'ludobox = ludocore:main'
         ],
-    },
-    license='GPL',
-    classifiers = [
-      "Programming Language :: Python",
-      "Environment :: Other Environment",
-      "Development Status :: 2 - Pre-Alpha",
-      "Intended Audience :: Developers",
-      "Operating System :: OS Independent",
-      "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    install_requires=required
-    )
+      },
+      license='GPL',
+      classifiers=[
+        "Programming Language :: Python",
+        "Environment :: Other Environment",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      install_requires=required
+      )
