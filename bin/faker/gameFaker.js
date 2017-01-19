4336c1251e8f30a7b120095b75049a89c36fcd71
Clément Renaud <clement.renaud@gmail.com>
2017-01-19 21:00:01 +0100

    add game faker

---
+++ bin/faker/gameFaker.js
@@ -0,0 +1,30 @@
+var jsf = require('json-schema-faker');
+var schema = require('../../ludobox/model/schema');
+var jsonfile = require('jsonfile');
+var slugify = require('slugify')
+var fs = require('fs');
+
+var ROOT_DIR = '../../data/';
+
+
+function createSampleGame() {
+  var sample = jsf(schema);
+  var slug = slugify(sample.title);
+
+  var dir = ROOT_DIR+slug;
+
+  if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+  }
+
+  var file = dir + "/info.json"
+
+  jsonfile.writeFile(file, sample, function (err) {
+    if (err) throw(err)
+    console.log('Sample file saved at '+file)
+  })
+}
+
+for (var i = 0; i < 10; i++) {
+  createSampleGame()
+}
