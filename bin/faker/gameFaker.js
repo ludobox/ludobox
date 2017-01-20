f89f953382c210ab0bacd91010091d7de93b5e04
Clément Renaud <clement.renaud@gmail.com>
2017-01-20 13:35:25 +0100

    game faker now add files

--- bin/faker/gameFaker.js
+++ bin/faker/gameFaker.js
@@ -7,6 +7,25 @@ var fs = require('fs');
 var ROOT_DIR = '../../data/';
 
 
+function writeFile(dir) {
+  var filename = Math.random().toString(36).substring(7);
+
+  var filepath = dir+"/"+filename;
+
+  fs.writeFile(filepath, "Hey there!", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("-- file saved! : " + filepath);
+  });
+}
+
+function writeRandomFiles(dir) {
+  for (var i = 0; Math.floor(Math.random()*10); i++) {
+    writeFile(dir)
+  }
+}
+
 function createSampleGame() {
   var sample = jsf(schema);
   var slug = slugify(sample.title).toLowerCase();
@@ -21,7 +40,13 @@ function createSampleGame() {
 
   jsonfile.writeFile(file, sample, function (err) {
     if (err) throw(err)
-    console.log('Sample file saved at '+file)
+    console.log('Sample info file saved at '+file)
+
+    if (fs.existsSync(dir)){
+        fs.mkdirSync(dir+"/files");
+        writeRandomFiles(dir+"/files")
+    }
+
   })
 }
 
