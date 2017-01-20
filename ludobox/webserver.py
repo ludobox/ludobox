c2ce65eb3e0f2d4f1652c2f295cf1ffd90334764
Clément Renaud <clement.renaud@gmail.com>
2017-01-20 14:04:01 +0100

    fix CORS function

--- ludobox/webserver.py
+++ ludobox/webserver.py
@@ -157,16 +157,16 @@ def create_resource():
     return jsonify({"path" : data_path}), 201
 
 # add unlimited CORS access
- -@app.after_request
- -def add_cors(resp):
- -    """ Ensure all responses have the CORS headers. This ensures any failures are also accessible
- -        by the client. """
- -    resp.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin','*')
- -    resp.headers['Access-Control-Allow-Credentials'] = 'true'
- -    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
- -    resp.headers['Access-Control-Allow-Headers'] = request.headers.get(
- -        'Access-Control-Request-Headers', 'Authorization' )
- -    # set low for debugging
- -    if app.debug:
- -        resp.headers['Access-Control-Max-Age'] = '1'
- -    return resp
+@app.after_request
+def add_cors(resp):
+    """ Ensure all responses have the CORS headers. This ensures any failures are also ccessible
+         by the client. """
+    resp.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin','*')
+    resp.headers['Access-Control-Allow-Credentials'] = 'true'
+    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+    resp.headers['Access-Control-Allow-Headers'] = request.headers.get(
+    'Access-Control-Request-Headers', 'Authorization' )
+    # set low for debugging
+    if app.debug:
+        resp.headers['Access-Control-Max-Age'] = '1'
+    return resp
