c7011caa8666c1460ae288eac07f6803e7298125
Clément Renaud <clement.renaud@gmail.com>
2017-02-06 18:43:20 +0100

    clarify text on home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -10,13 +10,13 @@ export default class Home extends React.Component {
         <p>What would you like to do ?</p>
         <ul>
           <li>
-            <a href="/games">Browse games</a>
+            <a href="/games">Browse games on this box</a>
           </li>
           <li>
             <a href="/create">Create a new game</a>
           </li>
           <li>
-            <a href="/download">Download more games</a>
+            <a href="/download">Download games on my box</a>
           </li>
           <li>
             <a href="/about">Learn more about the Ludobox project</a>
