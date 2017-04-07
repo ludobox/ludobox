a51fb008870bd2bce6cb455b0855f6cf27805bc7
Clément Renaud <clement.renaud@gmail.com>
2017-04-07 14:12:04 +0200

    fix failing Home test

--- client/src/components/Home/Home.test.js
+++ client/src/components/Home/Home.test.js
@@ -5,10 +5,20 @@ import { assert } from 'chai'
 
 import Home from './Home.jsx'
 
+const config = {
+  name : "My Test Box"
+}
+
 describe('<Home />', () => {
-  it('should say hello', () => {
-    const home = shallow(<Home />);
+  it('should say Hello', () => {
+    const home = shallow(<Home config={config}/>);
     const title = home.find('h3');
     assert.match(title.text(), /^Hello/)
+  })
+
+  it('should display the name of the box', () => {
+    const home = shallow(<Home config={config}/>);
+    const welcome = home.find('.welcome');
+    assert.include(welcome.text(), config.name)
   })
 })
