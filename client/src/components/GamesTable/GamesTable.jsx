b751b9f6b1856a7c9ee1688f2821b2070c08a278
Clément Renaud <clement.renaud@gmail.com>
2017-05-03 20:39:34 +0200

    add basic game filtering

--- client/src/components/GamesTable/GamesTable.jsx
+++ client/src/components/GamesTable/GamesTable.jsx
@@ -1,43 +1,64 @@
-import React from 'react'
+import React from 'react';
 
 import DownloadButton from '../RemoteGames/DownloadButton.jsx'
 
 export default class GamesTable extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      filterStr : ''
+    }
+  }
+
+  changeFilterStr(filterStr) {
+    this.setState({ filterStr : filterStr })
+  }
+
   render() {
-    console.log(this.props.games);
-    let rows = this.props.games.map( game => (
-      <tr style={ game.existsLocally ? { background : "yellow" } : {}  }
-        key={game.slug}>
-        <td>
-          <a
-            href={"/games/"+game.slug}
-            // target="_blank"
-            >
-            {game.title}
-          </a>
-        </td>
-        <td>{game.content_type}</td>
-        <td>{game.fabrication ? game.fabrication.fab_time : null}</td>
-        <td>{game.audience ? game.audience.language : null }</td>
-        {
-          ! game.existsLocally && this.props.remoteApi && this.props.localApi ?
+    // console.log(this.props.games);
+    let { games } = this.props
+    let { filterStr } = this.state
+
+    let rows = games
+      .filter(g => g.title.toLowerCase().includes(filterStr))
+      .map( game => (
+        <tr style={ game.existsLocally ? { background : "yellow" } : {}  }
+          key={game.slug}>
           <td>
-            <DownloadButton
-              socket={this.props.socket}
-              remoteApi={this.props.remoteApi}
-              localApi={this.props.localApi}
-              slug={game.slug}
-              />
+            <a
+              href={"/games/"+game.slug}
+              // target="_blank"
+              >
+              {game.title}
+            </a>
           </td>
-          :
-          null
-        }
-      </tr>
-    ))
+          <td>{game.content_type}</td>
+          <td>{game.fabrication ? game.fabrication.fab_time : null}</td>
+          <td>{game.audience ? game.audience.language : null }</td>
+          {
+            ! game.existsLocally && this.props.remoteApi && this.props.localApi ?
+            <td>
+              <DownloadButton
+                socket={this.props.socket}
+                remoteApi={this.props.remoteApi}
+                localApi={this.props.localApi}
+                slug={game.slug}
+                />
+            </td>
+            :
+            null
+          }
+        </tr>
+      ))
 
     return (
       <div>
+        <input
+          type="text"
+          value={ filterStr }
+          onChange={ e => this.changeFilterStr(e.target.value) }
+        />
         <table className="twelve columns" style={{tableLayout:"fixed"}}>
             <thead>
                 <tr>
