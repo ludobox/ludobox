24ea4f3c46696408684e3b2e4b42eda549295515
Clément Renaud <clement.renaud@gmail.com>
2017-07-13 01:24:34 +0200

    move login process to flask-security (less to maintain)

--- server/ludobox/models.py
+++ server/ludobox/models.py
@@ -2,71 +2,55 @@
 # -*- coding: utf-8 -*-
 #
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.ext.hybrid import hybrid_property
+from flask_security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
 
 from datetime import datetime
-
 from webserver import app
-
-from flask_bcrypt import Bcrypt
-
-# security
-bcrypt = Bcrypt(app)
 
 # db options
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# security
+app.config['SECURITY_REGISTERABLE'] = True
+app.config['SECURITY_PASSWORD_SALT'] = "dsdskjdksjdksjd&é"
+
 # setup DB
 db = SQLAlchemy(app)
 
-class User(db.Model):
+# Define models
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
     """An admin user capable of viewing reports.
 
     :param str email: email address of user
     :param str password: encrypted password for the user
 
     """
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
-    id = db.Column('user_id',db.Integer , primary_key=True)
+    id = db.Column(db.Integer , primary_key=True)
     username = db.Column('username', db.String(20), unique=True , index=True)
-    _password = db.Column('password' , db.String(128)    )
+    password = db.Column('password' , db.String(255))
     email = db.Column('email',db.String(50),unique=True , index=True)
-    registered_on = db.Column('registered_on' , db.DateTime)
 
-    def __init__(self , username, password , email):
-        self.username = username
-        self.password = password
-        self.email = email
-        self.registered_on = datetime.utcnow()
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
 
-    @hybrid_property
-    def password(self):
-        return self._password
+    registered_on = db.Column('registered_on' , db.DateTime())
 
-    @password.setter
-    def _set_password(self, plaintext):
-        self._password = bcrypt.generate_password_hash(plaintext)
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
-    def is_active(self):
-        """True, as all users are active."""
-        return True
-
-    def get_id(self):
-        """Return the unique id to satisfy Flask-Login's requirements."""
-        return unicode(self.id)
-
-    def is_authenticated(self):
-        """Return True if the user is authenticated."""
-        return self.authenticated
-
-    def is_anonymous(self):
-        """False, as anonymous users aren't supported."""
-        return False
-
-    def is_correct_password(self, plaintext):
-        return bcrypt.check_password_hash(self._password, plaintext)
-
-    def __repr__(self):
-        return '<User %r>' % (self.username)
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
--- server/ludobox/users.py
+++ server/ludobox/users.py
@@ -2,77 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from webserver import app
-
-from models import User, db, bcrypt
-
-from flask import Flask, Response, redirect, url_for, request, session, render_template, flash, jsonify
-from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
-
-# flask-login
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login"
+from models import User
 
 # config
 app.config.update(
     DEBUG = True,
     SECRET_KEY = 'secret_xxx'
 )
-
-@app.route('/register' , methods=['GET','POST'])
-def register():
-    if request.method == 'GET':
-        return render_template('register.html')
-    user = User(request.form['username'] , request.form['password'],request.form['email'])
-    db.session.add(user)
-    db.session.commit()
-    flash('User successfully registered')
-    return redirect(url_for('login'))
-
-@app.route('/login',methods=['GET','POST'])
-def login():
-    """For GET requests, display the login form. For POSTS, login the current user by processing the form."""
-    if request.method == 'GET':
-        return render_template('login.html')
-    print db
-
-    username = request.form['username']
-    password = request.form['password']
-    remember_me = False
-    if 'remember_me' in request.form:
-        remember_me = True
-
-    # validate user & password
-    user = User.query.filter_by(username=username).first_or_404()
-
-    if user.is_correct_password(password):
-        # do the actual login
-        login_user(user, remember = remember_me)
-    else:
-        flash('Username or Password is invalid' , 'error')
-        return redirect(url_for('login'))
-
-    flash('Logged in successfully')
-    return redirect(url_for('test'))
-
-# somewhere to logout
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return Response('<p>Logged out</p>')
-
-# callback to reload the user object
-@login_manager.user_loader
-def user_loader(user_id):
-    """Given *user_id*, return the associated User object.
-
-    :param unicode user_id: user_id (id) user to retrieve
-    """
-    print user_id
-    return User.query.get(user_id)
-
-@app.route('/test',methods=['GET'])
-@login_required
-def test() :
-    return Response('<p>Test</p>')
