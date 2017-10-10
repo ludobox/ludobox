656e5ff3f2892ecc1ac3971dc0e6f5095ee34b15
Clément Renaud <clement.renaud@gmail.com>
2017-10-10 22:49:29 +0200

    UI: fix roles ACL definition

--- client/src/roles.js
+++ client/src/roles.js
@@ -1,13 +1,26 @@
 
+let editor = {
+  "add_game" : true,
+  "edit_game" : true,
+  "validate_game" : true,
+  "reject_game" : true,
+  "back_to_review_game" : true,
+  "delete_game" : true
+}
+
 let contributor = {
   "add_game" : true,
   "edit_game" : true,
   "delete_own_game" : false,
-  "delete_others_game" : false
+  "delete_others_game" : false,
+  "validate_game" : false,
+  "reject_game" : false,
+  "back_to_review_game" : false
 }
 
 let acl = {
-  contributor
+  contributor,
+  editor
 }
 
 export function isAuthorized(action, user) {
