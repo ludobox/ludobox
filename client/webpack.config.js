9d3b0283dfe06fbf279305e1f21e3f6adcbbaf75
Clément Renaud <clement.renaud@gmail.com>
2017-10-16 21:59:53 +0200

    webpack support for font+better css handling

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -60,12 +60,12 @@ module.exports = {
         exclude: [node_modules_dir]
       },
       {
-        test: /\.css$/,
-        loader: 'style!css?modules'
+        test: /\.(scss|css)$/,
+        loaders: ['style', 'css', 'sass']
       },
       {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
       }
     ]
   },
