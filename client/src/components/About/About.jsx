547fd1e9432de3aeb775f5635f1fe93fcb26f1e3
Clement Renaud <clement.renaud@gmail.com>
2018-06-11 13:28:13 +0200

    About Page ready for translation

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -12,61 +12,57 @@ const messages = defineMessages({
       id: 'about.description.small',
       defaultMessage: 'Each box contains dozens of digital files and instructions to make your own games and turns any place into a game library.'
     },
-    printnplay : {
-      main : {
-        id : 'about.printnplay.main',
-        defaultMessage: "Print'n play are games that you can make by yourself."
-      },
-      line1: {
-        id : 'about.printnplyt.line1',
-        defaultMessage: "From a card game in PDF using a printer and scissors"
-      },
-      line2: {
-        id : 'about.printnplay.line2',
-        defaultMessage: "to a board game accessing a 3D printer to reproduce tokens,"
-      },
-      line3: {
-        id : 'about.printnplay.line3',
-        defaultMessage: "Ludobox invites to materialize & craft digital files."
-      }
+    printnplayMain : {
+      id : 'about.printnplay.main',
+      defaultMessage: "Print'n play are games that you can make by yourself."
     },
-    make: {
-      title : {
-        id: 'about.make.title',
-        defaultMessage: 'Make your own Box'
-      },
-      line1: {
-        id : 'about.make.line2',
-        defaultMessage: "You can build your own box from scratch"
-      },
-      line2: {
-        id : 'about.make.line3',
-        defaultMessage: "and directly download games from our online collection."
-      },
-      button : {
-        id : 'about.make.button',
-        defaultMessage: "Create your own"
-      },
-      links : {
-        id : 'about.make.links',
-        defaultMessage: 'Check <a href="https://github.com/ludobox/ludobox">the code</a> or read <a href="https://wiki.ludobox.net">the docs</a>'
-      }
+    printnplayLine1: {
+      id : 'about.printnplyt.line1',
+      defaultMessage: "From a card game in PDF using a printer and scissors"
     },
-    contact: {
-      wiki : {
-        id :'about.contact.wiki',
-        defaultMessage : 'You’d like to contribute to Ludobox platform ? Visit the <a href="https://wiki.ludobox.net">wiki</a>.'
-      }
+    printnplayLine2: {
+      id : 'about.printnplay.line2',
+      defaultMessage: "to a board game accessing a 3D printer to reproduce tokens,"
     },
-    footer: {
-      credits : {
-        id: 'about.footer.credits',
-        defaultMessage: 'This project is powered by <a className="no-external" href="http://dcalk.org" target="_blank"><b>DCALK</b>.</a>'
-      },
-      sponsors : {
-        id: 'about.footer.sponsors',
-        defaultMessage: 'It was made possible with the support of'
-      }
+    printnplayLine3: {
+      id : 'about.printnplay.line3',
+      defaultMessage: "Ludobox invites to materialize & craft digital files."
+    },
+    makeTitle : {
+      id: 'about.make.title',
+      defaultMessage: 'Make your own Box'
+    },
+    makeLine1: {
+      id : 'about.make.line2',
+      defaultMessage: "You can build your own box from scratch"
+    },
+    makeLine2: {
+      id : 'about.make.line3',
+      defaultMessage: "and directly download games from our online collection."
+    },
+    makeButton : {
+      id : 'about.make.button',
+      defaultMessage: "Create your own"
+    },
+    makeLinks : {
+      id : 'about.make.links',
+      defaultMessage: 'Check <a href="https://github.com/ludobox/ludobox">the code</a> or read <a href="https://wiki.ludobox.net">the docs</a>'
+    },
+    contactWiki : {
+      id :'about.contact.wiki',
+      defaultMessage : 'You’d like to contribute to Ludobox platform ? Visit the <a href="https://wiki.ludobox.net">wiki</a>.'
+    },
+    contactEmail : {
+      id :'about.contact.email',
+      defaultMessage : '<span id="contact-text">Contact us at :</span> <a href="mailto:contact@ludobox.net">contact@ludobox.net</a>'
+    },
+    footerCredits : {
+      id: 'about.footer.credits',
+      defaultMessage: 'This project is powered by <a className="no-external" href="http://dcalk.org" target="_blank"><b>DCALK</b>.</a>'
+    },
+    footerSponsors : {
+      id: 'about.footer.sponsors',
+      defaultMessage: 'It was made possible with the support of'
     }
   })
 
@@ -108,20 +104,20 @@ export default class AboutPage extends React.Component {
             />
           <h5>
             <span className="main">
-              <FormattedMessage {...messages.printnplay.main} />
+              <FormattedMessage {...messages.printnplayMain} />
             </span>
             <br />
             <small>
               <span className="line1">
-                <FormattedMessage {...messages.printnplay.line1} />
+                <FormattedMessage {...messages.printnplayLine1} />
               </span>
               <br />
               <span className="line2">
-                <FormattedMessage {...messages.printnplay.line2} />
+                <FormattedMessage {...messages.printnplayLine2} />
               </span>
               <br />
               <span className="line3">
-                <FormattedMessage {...messages.printnplay.line3} />
+                <FormattedMessage {...messages.printnplayLine3} />
               </span>
             </small>
           </h5>
@@ -136,27 +132,27 @@ export default class AboutPage extends React.Component {
           />
         <section id="make">
           <h3 className="title">
-            <FormattedMessage {...messages.make.title} />
+            <FormattedMessage {...messages.makeTitle} />
           </h3>
           <div className="container">
             <h5>
               <span className='line1'>
-                <FormattedMessage {...messages.make.line1} />
+                <FormattedMessage {...messages.makeLine1} />
               </span>
               <br />
               <span className="line2">
-                <FormattedMessage {...messages.make.line2} />
+                <FormattedMessage {...messages.makeLine2} />
               </span>
             </h5>
           </div>
           <p className="doclink">
             <a href="https://hackmd.io/s/Skje3bygW" className="button" target="_blank">
-              <FormattedMessage {...messages.make.button} />
+              <FormattedMessage {...messages.makeButton} />
             </a>
             <br />
           </p>
           <p className="links">
-            <FormattedHTMLMessage {...messages.make.links} />
+            <FormattedHTMLMessage {...messages.makeLinks} />
           </p>
         </section>
         <img className="full-width-image"
@@ -166,19 +162,19 @@ export default class AboutPage extends React.Component {
         <section id="contact" className="container" >
           <h3>Get in Touch</h3>
           <h5>
-            <FormattedHTMLMessage {...messages.contact.wiki} />
+            <FormattedHTMLMessage {...messages.contactWiki} />
             <br />
             <br />
-            <span id="contact-text">Contact us at :</span> <a href="mailto:contact@ludobox.net">contact@ludobox.net</a>
+            <FormattedHTMLMessage {...messages.contactEmail} />
           </h5>
         </section>
         <hr />
         <footer className="site-footer">
           <p className="support">
-            <FormattedHTMLMessage {...messages.footer.credits} />
+            <FormattedHTMLMessage {...messages.footerCredits} />
             <br />
             <small>
-              <FormattedMessage {...messages.footer.sponsors} />
+              <FormattedMessage {...messages.footerSponsors} />
             </small>
           </p>
           <ul id="logos">
